120667665ac4e0dc46aebe19989a9949a4b18cf7
Hanseok Seo <lovegkstjr01@naver.com>
2025-07-30 10:28:43 +0900

    :zap: test message 테스트해봅니다.

--- project01/src/main.jsx
+++ project01/src/main.jsx
@@ -11,6 +11,7 @@ import PowerCalc from './component/PowerCalc.jsx';
 import MyPage from './component/MyPage.jsx';
 import EditMyPage from './component/EditMyPage.jsx';
 
+// 테스트 해봅니다.
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
